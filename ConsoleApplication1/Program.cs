30ecee20cbe325aa351eff1a30dc137e5612b7aa
sahanes <sahanes_2000@yahoo.com>
2017-01-07 18:20:39 +0000

    refactoring classes into separate files

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,30 +7,6 @@ namespace TestObjectClass2
 {
  
 
-    public class BotFlange : Rectangle
-    {
-        public BotFlange(double width, double depth, double F_y)
-            : base(width, depth, F_y)
-        {
-            this.BotLocation = 0;
-            this.CG = 0;
-            this.TopLocation = 0;
-        }
-
-        public override double Area()
-        {
-            return x * y;
-        }
-        public override double I_x()
-        {
-            return x * Math.Pow(y, 3) / 12;
-        }
-        public override double I_y()
-        {
-            return y * Math.Pow(x, 3) / 12;
-        }
-    }
-
     public class Web_ : Rectangle
     {
         public Web_(double depth, double thickness, double F_y)
