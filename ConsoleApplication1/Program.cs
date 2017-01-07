020f5903fd3aafbd8c06cf1ae21e65f0af9dddaa
sahanes <sahanes_2000@yahoo.com>
2017-01-07 17:48:22 +0000

    start refactoring by extracting classes into individual files

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -5,31 +5,7 @@ using System.Text;
 
 namespace TestObjectClass2
 {
-    public abstract class Rectangle
-    {
-        public double x { get; set; }
-        public double y { get; set; }
-        public double strength { get; set; }
-        public double BotLocation { get; set; }
-        public double CG { get; set; }
-        public double TopLocation { get; set; }
-
-        public Rectangle(double x, double y, double strength)
-        {
-            this.x = x;
-            this.y = y;
-            this.strength = strength;
-            this.BotLocation = 0;
-            this.CG = 0;
-            this.TopLocation = 0;
-        }
-
-        public abstract double Area();
-        public abstract double I_x();
-        public abstract double I_y();
-
-    }
-
+ 
 
     public class BotFlange_ : Rectangle
     {
